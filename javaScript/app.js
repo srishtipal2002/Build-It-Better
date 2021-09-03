8ba049409cc2aa47a649a1a6c80144439a1e24b6
Srishti Pal <74287257+srishtipal2002@users.noreply.github.com>
2021-09-04 00:34:36 +0530

    Website

---
+++ javaScript/app.js
@@ -0,0 +1,401 @@
+products = [
+    {
+        catagory: 'chairs',
+        src: 'https://images.pexels.com/photos/2762247/pexels-photo-2762247.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        itemName: 'Black Chair',
+        description: "You can stack the chairs, so they take less space when you're not using them. For maximum quality, re-tighten the screws when necessary. This chair has been tested for home use and meets the requirements for durability and safety.",
+        price: "1,299 (Inclusive of all taxes)",
+    },
+	
+    {
+        catagory: 'sofas',
+        src: 'https://images.pexels.com/photos/1571460/pexels-photo-1571460.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        itemName: '5 Seater Sofa cum Bed',
+        description: 'This sofa was constructed featuring handpicked Premium upholstery with excellent finish. Made from high grade Solid wood. The wood used in the furniture is perfectly seasoned for optimum moisture content; to reduce the possibility of seasonal expansion or contraction of the products.',
+        price: '48,999 (Inclusive of all taxes)',
+    },
+
+    {
+        catagory: 'chairs',
+        src: 'https://images.pexels.com/photos/2258083/pexels-photo-2258083.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        itemName: 'Blue Velvet Chair',
+        description: 'Upgrade your dining space with this table from home centre which features minimal yet modern design. It uses a combination of vinyl, steel, plastic, glass or wood having monochromatic colours and sleek silhouettes.',
+        price: '12,999 (Inclusive of all taxes)',
+    },
+	
+    {
+        catagory: 'chairs',
+        src: 'https://images.pexels.com/photos/2180883/pexels-photo-2180883.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        itemName: 'Classy Bar Chair',
+        description: 'A very comfortable height adjustable counter height bar chair/stool, perfect for home or work place. Black Leatherette + Rebound sponge cushion seat, comfortable and soft feel, comfortable armrest. Metal lifting handle, alloy lift gas bar, safe and reliable, both can lift or 360° rotation.',
+        price: '7,999 (Inclusive of all taxes)',
+    },
+	
+    {
+        catagory: 'sofas',
+        src: 'https://5.imimg.com/data5/LS/NL/KU/SELLER-12803526/brown-indian-royal-furniture-500x500.jpg',
+        itemName: 'Royal Sofa',
+        description: 'Very premium quality which enhances its life durable and long lasting. These are bright and colourful which easily matches with any type of room and its ambience. They beatify our living room with its attractive look and embrace the surroundings.',
+        price: '1,79,999 (Inclusive of all taxes)',
+    },
+	
+    {
+        catagory: 'sofas',
+        src: 'https://images.pexels.com/photos/1866149/pexels-photo-1866149.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        itemName: 'Modern Two Seater Sofa',
+        description: 'Modern Style Sofas usually come in fixed back with cushions attached to the frame. They mostly come with minimal details & compact proportions. These sofas can be with or without arms.',
+        price: '14,999 (Inclusive of all taxes)',
+    },
+	
+    {
+        catagory: 'chairs',
+        src: 'https://images.pexels.com/photos/1350789/pexels-photo-1350789.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        itemName: 'Twin Chair Set with Table',
+        description: 'Ergonomically conscious, this Dining Chair features a sturdy rubber wood frame, PVC upholstered seat and back and a durable fold & stack feature, making it a stylish pick.',
+        price: '15,999 (Inclusive of all taxes)',
+    },
+	
+    {
+        catagory: 'sofas',
+        src: 'https://images.pexels.com/photos/276583/pexels-photo-276583.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        itemName: 'Classic Leather Black Sofa',
+        description: 'This sofa was constructed featuring handpicked Premium upholstery with excellent finish. Made from high grade Solid wood. The wood used in the furniture is perfectly seasoned for optimum moisture content; to reduce the possibility of seasonal expansion or contraction of the products.',
+        price: '49,999 (Inclusive of all taxes)',
+    },
+	
+    {
+        catagory: 'diningTable',
+        src: 'https://images.pexels.com/photos/271696/pexels-photo-271696.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        itemName: 'Classic 4 Seater Dining Table',
+        description: 'Upgrade your dining space with this table from home centre which features minimal yet modern design. It uses a combination of vinyl, steel, plastic, glass or wood having monochromatic colours and sleek silhouettes.',
+        price: '24,999 (Inclusive of all taxes)',
+    },
+	
+    {
+        catagory: 'diningTable',
+        src: 'https://images.pexels.com/photos/3356416/pexels-photo-3356416.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        itemName: 'Sofa Type 4 Seater',
+        description: 'Deep sofa seat for unexpected comfort and modern styling fits coordinates well with most decor. Timeless modern design of sofa chair can match perfectly with any decor theme and its the perfect addition to any living room environment.',
+        price: '43,999 (Inclusive of all taxes)',
+    },
+	
+    {
+        catagory: 'decor',
+        src: 'https://images.pexels.com/photos/139764/pexels-photo-139764.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        itemName: 'Painting Set of 4',
+        description: ' Without glass for minimal glare, lightweight and durable. Easy to use and ready to hang. A great gift idea for your relatives, friends and family. Perfect for home decor, wall decoration. Painting for living room, stairs, bed room, dining room, office, hotel, office, bar, gym, etc.',
+        price: '2,999 (Inclusive of all taxes)',
+    },
+	
+    {
+        catagory: 'diningTable',
+        src: 'https://images.pexels.com/photos/534172/pexels-photo-534172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        itemName: 'Six Seator Dining Table',
+        description: 'Cozy and compact dining now at hand with the solid wood Arabian dining table.  The stylised legs and compact design make this an easy fit into any space. It seats six people comfortably. It is paired with 6 Pearl Dining Chairs for an elegant dining set. The Chair is made from Solid wood and has an upholstered seat for added comfort.',
+        price: '41,999 (Inclusive of all taxes)',
+    },
+	
+    {
+        catagory: 'decor',
+        src: 'https://images.pexels.com/photos/1470168/pexels-photo-1470168.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        itemName: 'Plant Set',
+        description: "Decorating artificial plants with pot for home decor are best at one's home, office, study room, Bathroom, Club decoration. The Product is made from high quality plastic grass sourced from the industry's best Manufacturers to give you almost satisfaction For Best Quality. Best for table, wall shelves decor and home decoration.",
+        price: '1,049 (Inclusive of all taxes)',
+    },
+	
+    {
+        catagory: 'decor',
+        src: 'https://images.pexels.com/photos/1129413/pexels-photo-1129413.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        itemName: 'Stylish Lamps',
+        description: 'Wooden Handmade Floor Lamp Decor (Wire and Bulb Included). Excellent Home Decor. Suitable for your Bedroom Drawing Room Hallway. Looks very beautiful in dark room.',
+        price: '5,049 (Inclusive of all taxes)',
+    },
+	
+    {
+        catagory: 'decor',
+        src: 'http://cdn.shopify.com/s/files/1/0276/4911/4190/products/61MlhWGsRbL.jpg?v=1602354803',
+        itemName: 'Chandelier / Jhoomar',
+        description: "Instantly upgrade your dining room, entry way, or kitchen with this chic chandelier, which features light-catching glass and a spherical design. The matte black finish gives off a slight gleam and doesn't show dust as easily as other finishes. The candelabra bulbs offer the look of an older world look for a throwback to simpler times.",
+        price: '12,999 (Inclusive of all taxes)',
+    },
+	
+    {
+        catagory: 'cupboard',
+        src: 'https://images.woodenstreet.de/image/data%2Fchest-of-drawers-ws%2Fadolph-chest-of-drawers%2Frevised%2Fhoney%2Ffront.jpg',
+        itemName: 'Stylish Wooden Chest of Drawers',
+        description: 'A little bit modern and a little bit classic. Blends warm wood tones and transitional lines with a lightweight structure and sleek wood hardware. The drawer chest is a versatile piece that can be used all over the home, from the living room to the bedroom.',
+        price: '20,999 (Inclusive of all taxes)',
+    },
+	
+	{
+        catagory: 'beds',
+        src: 'https://i.pinimg.com/originals/25/3b/07/253b07f3c1de029e370720f06033a9ac.jpg', 
+		itemName: 'Canopy Bed',
+        description: " Canopy bed with storage is rustic furniture design. The wooden furniture also has metal panels patterned right upon it, and the fine details embellish it more. It is made up of Sheesham wood. There are finishes of honey, teak, walnut, and mahogany available with the furniture.",
+        price: "17,999 (Inclusive of all taxes)",
+    },
+	
+	{
+        catagory: 'beds',
+        src: 'https://sc04.alicdn.com/kf/H24a95fc35a1842be85d82295c92a32ccu.jpg',
+		itemName: 'Double Iron Steel Metal Bed',
+        description: "Stylish and leisure, comfortable seating and sleeping, strong structure and Stable,keep go shape for longterm, Sponge: Fireproof high density, practical and multifunctionaland can be folding freely,",
+        price: "6,999 (Inclusive of all taxes)",
+    },
+	
+	{
+        catagory: 'beds',
+        src: 'https://aarsunwoods.b-cdn.net/wp-content/uploads/2020/03/Antique-Style-Wooden-Bed-UH-BED-0001.jpg',
+		itemName: 'Antique Style Wooden Bed',
+        description: "Taking inspiration from Regal Culture, This Antique Style Wooden Bed is perfect for creating a luxurious and majestic ambiance in your bedroom decor. Throughout this beautiful furniture piece, you will find gorgeous and breathtaking carvings that lend an old-world charm and elegance. The Antique Style Wooden bed became the canvas for inspiring carvings and design interpretations in different upscale residences. This Handmade Wooden Bed is made by premium quality Teak Wood.",
+        price: "1,20,000 (Inclusive of all taxes)",
+    },
+	
+	{
+        catagory: 'cupboard',
+        src: 'https://ii1.pepperfry.com/media/catalog/product/r/i/568x625/riku-4-door-wardrobe-with-2-drawers-in-wenge-finish-by-mintwud-riku-4-door-wardrobe-with-2-drawers-i-jyqpwe.jpg',
+		itemName: '4 Door Wardrobe',
+        description: "Classic amalgamation of form and function. Crafted for compact homes, the range is clean and convenient and has an understated design aesthetic that adapts to any space. Wardrobe is closet where you can either hang your clothes upright or stack them on horizontal shelves. They come in a variety of wood types and in either options of 1,2,3,4 + doors, with or without mirrors and drawers. They are handy space saving armoires.",
+        price: "14,999 (Inclusive of all taxes)",
+    },
+	
+	{
+        catagory: 'cupboard',
+        src: 'https://ii1.pepperfry.com/media/catalog/product/e/l/568x625/ella-dressing-unit-in-light-walnut-finish-by-trendsbee-ella-dressing-unit-in-light-walnut-finish-by--k4ugdp.jpg',
+		itemName: 'Classic Wooden Dressing Table',
+        description: "World-class Dressing Table with storage, born from a convergence of great design, product innovation, craftsmanship, and international furniture proficiency. A Dresseris a dressing table that has chest of drawers as the base and usually a mirror on the top and additional stool provided.",
+        price: "22,999(Inclusive of all taxes)",
+    },
+	
+
+]
+
+let cards = document.querySelector('.items');
+update('all');
+getButtons();
+
+let green = document.querySelector('.green');
+let red = document.querySelector('.red');
+
+green.style.display = "none";
+red.style.display = "none";
+
+function getButtons() {
+    let buttons = document.querySelectorAll('.addToCart');
+    buttons.forEach((btn) => {
+        // console.log(btn);
+        btn.addEventListener('click', (e) => {
+            console.log('clicked');
+            let item = e.target.parentElement.cloneNode(true);
+            let heading = item.querySelector('h2').innerText;
+            let price = item.querySelector('.price').innerText;
+
+            if (sessionStorage.getItem('cartShopperz') == null) {
+                let cartItems = []
+                cartItems.push([heading, price])
+                sessionStorage.setItem('cartShopperz', JSON.stringify(cartItems));
+            }
+            else {
+                let cartItems = sessionStorage.getItem('cartShopperz');
+                let items = JSON.parse(cartItems);
+                items.push([heading, price]);
+                sessionStorage.setItem('cartShopperz', JSON.stringify(items));
+            }
+            showCartItems();
+            green.style.display = "flex";
+            setTimeout(() => {
+                green.style.display = "none";
+            }, 2000);
+        });
+    });
+}
+
+let cart = document.querySelector('.cart');
+
+let togglers = document.querySelectorAll('.toggleBtns');
+togglers.forEach((btntog) => {
+    btntog.addEventListener('click', () => {
+        update(btntog.id);
+        getButtons();
+    })
+});
+
+function update(id) {
+    if (id == 'all') {
+        let str = ''
+        for (let i = 0; i < products.length; i++) {
+            const element = products[i];
+            str += `<div class="card">
+                <img src=${element.src}
+                    alt="image" class="item-image">
+                <h2>${element.itemName}</h2>
+                <p>${element.description}</p>
+                <h3 class="price">₹${element.price}</h3>
+                <button class="addToCart">Add To Cart</button>
+            </div>`;
+        }
+        cards.innerHTML = str;
+    } else if (id == 'chairs' || id == 'sofas' || id == 'diningTable' || id == 'decor' || id=='beds' || id=='cupboard' ) {
+        let str = ''
+        for (let i = 0; i < products.length; i++) {
+            const element = products[i];
+            if (element.catagory == id) {
+                str += `<div class="card">
+                    <img src=${element.src}
+                        alt="image" class="item-image">
+                    <h2>${element.itemName}</h2>
+                    <p>${element.description}</p>
+                    <h3 class="price">₹${element.price}</h3>
+                    <button class="addToCart">Add To Cart</button>
+                </div>`;
+            }
+        }
+        cards.innerHTML = str;
+    }
+}
+
+let counter = document.getElementById('counter');
+console.log(counter);
+var count = 0;
+function showCartItems() {
+    let a = sessionStorage.getItem('cartShopperz');
+    let items = JSON.parse(a);
+    // console.log(items);
+    count = 0;
+    if (items != null) {
+        let str = ''
+        items.forEach((element, index) => {
+            let heading = element[0];
+            let price = element[1];
+            str += `<div class="cart-item">
+                                <h2>${heading}</h2>
+                                <h2><p>${price}</p></h2>
+                                <button class="deleteItem" onclick="deleteItemInCart(${index})">X</button>
+                            </div>`;
+            count++;
+
+        });
+        if (items.length != 0) {
+            str += `<button id="check"><a href="Payment/PaymentGateway.html" target="_self"><h1>Checkout</h1></a></button>`;
+            counter.innerText = count;
+        } else {
+            str += `<p>Nothing in the Cart!!!</p>`;
+            counter.innerText = count;
+        }
+        cart.innerHTML = str;
+    }
+}
+
+function deleteItemInCart(index) {
+    console.log('delete');
+    let a = sessionStorage.getItem('cartShopperz');
+    let items = JSON.parse(a);
+    items.splice(index, 1);
+    sessionStorage.setItem('cartShopperz', JSON.stringify(items));
+    count--;
+    showCartItems();
+    red.style.display = "flex";
+    setTimeout(() => {
+        red.style.display = "none";
+    }, 2000);
+}
+
+cart.style.display = 'none';
+
+let cartT = document.getElementById('cart');
+let cartBtn = document.querySelector('.cartBtn');
+
+// console.log(cartBtn);
+cartBtn.addEventListener('click', () => {
+    if (cartT.style.display == 'none') {
+        cartT.style.display = 'flex';
+    }
+    else if (cartT.style.display == 'flex') {
+        cartT.style.display = 'none';
+    }
+});
+
+showCartItems();
+
+reviews = [
+    {
+        Name: 'Pankaj Khanna',
+        src: 'https://picsum.photos/200/300?random=1',
+        desc: 'I was quite surprised with the speed of delivery and assembling received. As it was urgent requirements. It got my bed and sofa delivered within 2 days. By luck or grace. But I got on time. Thank your very much and I appreciate same',
+    },
+    {
+        Name: 'Jaydeep Krishna',
+        src: 'https://picsum.photos/200/300?random=2',
+        desc: 'Hi I bought furniture from Build it better a year back and since that time I feel great about the quality and design of the furniture. I recommend Build it better to my family and relatives',
+    },
+    {
+        Name: 'Bhuvan Bam',
+        src: 'https://picsum.photos/200/300?random=3',
+        desc: 'Best site to buy furniture. After searching many shops in my city, someone suggested me to buy online at Build It Better and I think it was my best decision to shop through their website',
+    },
+    {
+        Name: 'Sandeep Maheshwari',
+        src: 'https://picsum.photos/200/300?random=4',
+        desc: 'Very neatly carved piece and very reasonably priced for the workmanship. Such handcrafted products deserve a much better price.',
+    },
+    {
+        Name: 'Utsav Punia',
+        src: 'https://picsum.photos/200/300?random=5',
+        desc: 'Superb site for all the household needs. Found some interesting and quirky stuff for a home like this fabulous compressed Marble dining table. Highly recommend visiting this store.',
+    },
+]
+
+let prev = document.querySelector('.prev');
+let next = document.querySelector('.next');
+// console.log(prev, next);
+let review = document.querySelector('.review');
+var index = 0;
+updateReview(index);
+prev.addEventListener('click', () => {
+    index = index - 1;
+    if (index < 0) {
+        index = reviews.length - 1;
+        updateReview(index);
+    } else {
+        updateReview(index);
+    }
+    console.log(index);
+});
+
+next.addEventListener('click', () => {
+    index = index + 1;
+    if (index > 4) {
+        index = 0
+        updateReview(index);
+    } else {
+        updateReview(index);
+    }
+    console.log(index);
+});
+
+function updateReview(num) {
+    let ele = reviews[num];
+    review.innerHTML = `<h1>${ele.Name}</h1>
+                        <img src=${ele.src} alt="images">
+                        <p>${ele.desc}</p>`;
+}
+
+let navItems = document.getElementById('items-nav');
+let navbarToggle = document.getElementById('menu-toggle');
+if (screen.width <= 660) {
+    navItems.style.display = "none";
+}
+
+console.log(navItems, navbarToggle);
+navbarToggle.addEventListener('click', () => {
+    console.log('click');
+    if (navItems.style.display == "none") {
+        navItems.style.display = "flex";
+        navbarToggle.style.transform = 'rotate(90deg)';
+    }
+    else if (navItems.style.display == "flex") {
+        navItems.style.display = "none";
+        navbarToggle.style.transform = 'rotate(0deg)';
+    }
+})
+
---
+++ readme.md
@@ -0,0 +1,23 @@
+# Build it better
+
+## This website is a design template of a e-commerce website
+
+## This website is designed by using
+- HTML
+- CSS
+- Javascript
+
+## This website is responsive on all the devices from a Laptop to an i Phone
+
+### Desktop
+![alt text]('./images/desktop1.png')
+![alt text]('./images/desktop2.png')
+
+### Smartphone
+![alt text]('./images/mobile1.png')
+![alt text]('./images/mobile2.png')
+
+#### I dont own any of the vectors used in the website except my logo
+
+
+##### Made by Build It Better Team
